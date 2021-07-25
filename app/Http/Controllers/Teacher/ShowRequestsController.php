408b3e9f9d8218bfae816c858a5c865c360cd134
Mostafa Rabia <mostafarabia64@gmail.com>
2021-07-25 23:46:00 +0200

    edit

--- app/Http/Controllers/Teacher/ShowRequestsController.php
+++ app/Http/Controllers/Teacher/ShowRequestsController.php
@@ -15,11 +15,11 @@ class ShowRequestsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $get_reads = $request->user()->reads;
+        $get_reads = $request->user()->reads_save;
 
         return ModelsRequest::where(function ($query) use ($get_reads) {
             $query->whereIn('read', $get_reads)->orWhereHas('user', function ($query) use ($get_reads) {
-                $query->whereIn('read', $get_reads);
+                $query->whereIn('reads_save', $get_reads);
             });
         })->simplePaginate(5);
     }
