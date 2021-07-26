3cae7cd336393b7602873984680253d2c968039f
Mostafa Rabia <mostafarabia64@gmail.com>
2021-07-26 18:17:34 +0200

    edit

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -10,7 +10,7 @@ class ContactUs extends Model
     use HasFactory;
 
     protected $casts = [
-        'email' => 'encrypt',
+        'email' => 'encrypted',
         'is_showed' => 'boolean',
     ];
 
