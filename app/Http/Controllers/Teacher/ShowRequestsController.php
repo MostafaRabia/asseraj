b32ff8c665a833d24d785168d52658c370477546
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-02 16:33:33 +0200

    edit

--- app/Http/Controllers/Teacher/ShowRequestsController.php
+++ app/Http/Controllers/Teacher/ShowRequestsController.php
@@ -15,10 +15,25 @@ class ShowRequestsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $get_reads = array_merge($request->user()->reads_save ?: [],$request->user()->reads_learning ?: []);
+        $teacher = $request->user();
+        $get_reads = array_merge($teacher->reads_save ?: [],$teacher->reads_learning ?: []);
+        $permissions = [
+            'work_in_qra2at' => 'learn',
+            'work_in_save' => 'check',
+        ];
+        $columns = [];
+
+        foreach($permissions as $permission => $var){
+            if ($teacher->hasPermission($permission)){
+                $columns[] = $var;
+            }
+        }
 
         return ModelsRequest::where(function ($query) use ($get_reads) {
             $query->whereIn('read', $get_reads);
-        })->with('student')->simplePaginate(5);
+        })
+        ->with('student')
+        ->whereIn('type',$columns)
+        ->simplePaginate(5);
     }
 }
