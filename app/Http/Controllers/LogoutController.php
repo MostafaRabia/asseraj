d84b6709279186802ebd1f0cc923b10092812968
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-19 16:28:22 +0200

    edit

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class LogoutController extends Controller
 {
     public function logout(Request $r){
-        \Log::info((array) $r->user()->currentAccessToken());
+        \Log::info($r->header('Authorization'));
+        \Log::info($r->user()->tokens());
         $r->user()->tokens()->delete();
     }
 }
