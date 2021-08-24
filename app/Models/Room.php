5899f1b017e5a2ee3888f9d5d96da7f335acb1cd
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-24 19:00:23 +0200

    edit

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,9 +12,12 @@ class Room extends Model
 
     public $guarded = [];
 
-    protected $casts = [
-        'created_at' => 'datetime:j/n/Y g:i A',
-    ];
+    protected $appends = ['created_at'];
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->timezone(optional(auth()->user())->timezone)->format('j/n/Y g:i A');
+    }
 
     public function student()
     {
