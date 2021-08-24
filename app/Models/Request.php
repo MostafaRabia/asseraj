ea8bddc474942ebe73e71dae135b8f6c942392d7
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-24 19:13:40 +0200

    edit

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,13 @@ class Request extends Model
     use HasFactory;
 
     public $guarded = [];
+
+    protected $appends = ['created_at'];
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->timezone(optional(auth()->user())->timezone)->format('j/n/Y g:i A');
+    }
 
     public function student()
     {
