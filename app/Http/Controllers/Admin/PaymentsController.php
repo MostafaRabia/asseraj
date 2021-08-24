1477c57d38d44c675b57e3135d097c272dfdaf63
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-24 17:30:21 +0200

    edit

--- app/Http/Controllers/Admin/PaymentsController.php
+++ app/Http/Controllers/Admin/PaymentsController.php
@@ -15,6 +15,18 @@ class PaymentsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return Payment::with('user:id,first_name,last_name')->paginate(10);
+        $payments = new Payment();
+        $payments = $payments->with('user:id,first_name,last_name');
+        $search = $request->input('query');
+
+        if ($search != null){
+            $payments->where(function($q) use ($search){
+                $q->whereHas('user',function($q) use ($search){
+                    $q->whereRaw("concat(first_name, ' ', last_name) like '%".$search."%'");
+                })->orWhere('invoice_id','LIKE',$search);
+            });
+        }
+
+        return $payments->paginate(10);
     }
 }
