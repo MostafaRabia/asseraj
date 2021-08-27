808cbd14921a89e2ab65ccc9b8356b271c3e3221
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-27 21:17:19 +0200

    edit

--- app/Traits/CurrencyTrait.php
+++ app/Traits/CurrencyTrait.php
@@ -17,6 +17,7 @@ trait CurrencyTrait
 
         $currency = Http::get('http://data.fixer.io/api/latest?access_key='.config('fixer.access_token').'&format=1');
         $currency = $currency->json();
+        $currency = $currency['rates'];
 
         $rate = $currency[$to] / $currency[$from];
 
