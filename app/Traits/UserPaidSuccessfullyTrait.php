8001d8f1919271ddd1a691b9c4753611673d33d7
Mostafa Rabia <mostafarabia64@gmail.com>
2021-08-27 21:40:55 +0200

    edit

--- app/Traits/UserPaidSuccessfullyTrait.php
+++ app/Traits/UserPaidSuccessfullyTrait.php
@@ -7,5 +7,9 @@ use App\Models\User;
 trait UserPaidSuccessfullyTrait{
     function paid($pay){
         User::where('id',$pay->user_id)->increment('minutes', $pay->minutes);
+
+        return response()->json([
+            'status' => 'done',
+        ],200);
     }
 }
