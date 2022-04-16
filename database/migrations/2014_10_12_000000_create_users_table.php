4af1ceebc52b5c231c54ba44c5065c23e7a3a30a
Mostafa Rabia <mostafarabia64@gmail.com>
2022-04-16 14:17:31 +0200

    edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name', 30);
             $table->string('last_name', 30);
-            $table->string('email');
+            $table->text('email');
             $table->string('emailsig', 30)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
