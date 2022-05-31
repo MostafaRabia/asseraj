cf269ef59550821bad38d9ba487a25d786f244c3
Mostafa Rabia <mostafarabia64@gmail.com>
2022-05-31 17:07:58 +0200

    change in db

---
+++ database/migrations/2022_05_31_150632_edit_in_lengths_of_contact_us.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditInLengthsOfContactUs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            $table->text('email')->change();
+            $table->string('subject', 255)->change();
+            $table->string('phone', 255)->change();
+            $table->string('name', 60)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            //
+        });
+    }
+}
